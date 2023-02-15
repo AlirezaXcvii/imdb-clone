d96862e42bd3c22f0fe086414387e2dcb1f7e76c
Arx <alireza.xcvii@gmail.com>
2023-02-16 01:47:20 +0330

    first

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-cyan-300'>
+        Hi
+      </h1>
+    </div>
+  )
+}
